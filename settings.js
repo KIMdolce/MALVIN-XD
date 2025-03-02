fd486483d746e3fca274467e238da0a738494d2c
KIMdolce <kimdolce36@gmail.com>
2025-03-02 16:44:55 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~pkQkRQbA#pKFegamhWK1O85JhpQ_spD81s4--970nOn9Xlj9HzIg",
+SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~spw0hSjZ#rZBBYzN0I9CJszhGVJ4ydrGfCpkNOEq3Xw0GpO8ObTM",
 // add your Session Id 
 
 PREFIX: process.env.PREFIX || ".",
@@ -26,7 +26,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "263714757857",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "50942117991",
 // add your bot owner number
 
 OWNER_NAME: process.env.OWNER_NAME || "✦ᴍᴀʟᴠɪɴ ᴛᴇᴄʜ🪀✦",
@@ -107,7 +107,7 @@ FAKE_TYPING: process.env.FAKE_TYPING || "false",
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
 
-DEV: process.env.DEV || "263714757857",
+DEV: process.env.DEV || "50942117991",
 //replace with your whatsapp number       
  
 ANTI_VV: process.env.ANTI_VV || "true",
